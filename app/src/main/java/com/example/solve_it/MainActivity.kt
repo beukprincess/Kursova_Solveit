94fc4148e05e72eb72c022df268f9657b3231a18
dest-1ny <romansioli89@gmail.com>
2025-04-28 17:46:34 +0300

    imageView_visability

--- app/src/main/java/com/example/solve_it/MainActivity.kt
+++ app/src/main/java/com/example/solve_it/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.solve_it
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
@@ -11,10 +12,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var button1: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         Thread.sleep(3000)
         installSplashScreen()
         setContentView(R.layout.registration_page)
+        val actionBar: ActionBar? = supportActionBar
+        if (actionBar != null) {
+            actionBar.hide()
+        }
 
         button1 = findViewById<Button>(R.id.log_in_button)
 
