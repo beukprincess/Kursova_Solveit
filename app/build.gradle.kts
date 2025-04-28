f838120f9092ba9ed3ff01c4bb37d9f245692af2
dest-1ny <romansioli89@gmail.com>
2025-04-28 20:03:06 +0300

    image_diplay_fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
